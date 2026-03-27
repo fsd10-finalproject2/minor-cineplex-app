1d9fb3e822b146d05b60bce85fd38528ed5af46b
James <jamespond707@gmail.comm>
2026-03-27 16:10:13 +0700

    feat: introduce new UI components for pagination and tab navigation, including `CaratButton`, `CustomPagination`, and `TabGroup`.

--- src/components/ui/CaratButton.vue
+++ src/components/ui/CaratButton.vue
@@ -11,7 +11,7 @@ defineProps<{
   <button
     :disabled="disabled"
     @click="$emit('click', $event)"
-    class="p-2 rounded-lg transition-all duration-200 text-gray-200 enabled:hover:text-white enabled:hover:bg-gray-100/10 disabled:opacity-20 disabled:cursor-not-allowed cursor-pointer flex items-center justify-center shrink-0"
+    class="p-2 w-10 h-10 rounded-full transition-all duration-200 text-gray-200 enabled:hover:text-white enabled:hover:bg-gray-100/10 disabled:opacity-20 disabled:cursor-not-allowed cursor-pointer flex items-center justify-center shrink-0"
   >
     <ChevronLeftIcon v-if="direction === 'left'" class="size-6" />
     <ChevronRightIcon v-if="direction === 'right'" class="size-6" />
