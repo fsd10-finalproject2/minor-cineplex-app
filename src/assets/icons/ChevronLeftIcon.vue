32438de67821c166d17715824d1d476f5488a172
James <jamespond707@gmail.comm>
2026-03-27 16:43:27 +0700

    feat: introduce custom pagination component and a chevron left icon

--- src/assets/icons/ChevronLeftIcon.vue
+++ src/assets/icons/ChevronLeftIcon.vue
@@ -5,18 +5,18 @@ const props = withDefaults(
     color?: string
   }>(),
   {
-    size: 16,
+    size: 24,
     color: 'currentColor',
   },
 )
 </script>
 
 <template>
-  <svg :width="props.size" :height="props.size" viewBox="0 0 8 13" fill="none">
+  <svg :width="props.size" :height="props.size" viewBox="0 0 24 24" fill="none" v-bind="$attrs">
     <path
-      d="M6.70703 0.353516L0.707031 6.35352L6.70703 12.3535"
+      d="M15 18L9 12L15 6"
       :stroke="props.color"
-      stroke-width="1.5"
+      stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
     />
