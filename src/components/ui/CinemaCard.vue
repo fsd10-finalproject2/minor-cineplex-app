9ab9d12e8b650f765937a3a71ab3a66c7a4375f5
James <jamespond707@gmail.comm>
2026-03-30 15:18:40 +0700

    feat: add CinemaCard component for displaying cinema location details

--- src/components/ui/CinemaCard.vue
+++ src/components/ui/CinemaCard.vue
@@ -9,17 +9,17 @@ defineProps<{
 </script>
 
 <template>
-  <div class="flex items-center w-full max-w-[590px] min-h-[90px] p-3 md:p-4 rounded border border-gray-100 gap-3 md:gap-4 transition-all hover:bg-blue-100/10 cursor-pointer">
+  <button type="button" class="flex items-center w-full max-w-[590px] min-h-[90px] p-3 md:p-4 rounded border border-gray-100 gap-3 md:gap-4 transition-all hover:bg-blue-100/10 text-left">
     <!-- Icon Container -->
     <div class="w-12 h-12 rounded-full bg-gray-100 flex items-center justify-center shrink-0">
-      <PinIcon class="size-6 text-blue-100" />
+      <PinIcon class="size-8 text-blue-100" />
     </div>
 
     <!-- Content -->
     <div class="flex flex-col gap-1 overflow-hidden">
-      <h3 class="style-headline-3 text-white truncate">
+      <span class="style-headline-3 text-white truncate">
         {{ name }}
-      </h3>
+      </span>
 
       <div class="flex items-center gap-2 style-body-2-regular text-gray-400 truncate">
         <template v-if="distance">
@@ -29,7 +29,7 @@ defineProps<{
         <p class="truncate opacity-70">{{ address }}</p>
       </div>
     </div>
-  </div>
+  </button>
 </template>
 
 <style scoped>
