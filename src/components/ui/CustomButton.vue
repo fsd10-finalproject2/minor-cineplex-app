076baef674e34a3e8c661cab94be02d036b53545
fsd10-finalproject2 <fsd10.finalproject2@gmail.com>
2026-04-02 18:26:29 +0700

    Update CustomButton.vue

--- src/components/ui/CustomButton.vue
+++ src/components/ui/CustomButton.vue
@@ -27,17 +27,17 @@ const variantMap = {
   `,
 
   secondary: `
-    bg-transparent border border-gray-300 text-black py-2 px-7
+    bg-transparent border border-gray-300  py-2 px-7
     enabled:hover:bg-gray-200 enabled:active:bg-gray-300
-    dark:text-white dark:border-gray-600
+    text-white dark:border-gray-600
     enabled:dark:hover:bg-gray-700 enabled:dark:active:bg-gray-600
     disabled:opacity-50 disabled:cursor-not-allowed 
   `,
 
   ghost: `
-    text-black underline px-0
+    underline px-0
     enabled:hover:text-gray-600 enabled:active:text-gray-800
-    dark:text-white enabled:dark:hover:text-gray-300
+    text-white enabled:dark:hover:text-gray-300
     disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:text-black  disabled:dark:hover:text-white
   `,
 } as const
