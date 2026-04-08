d7d61a5aaff093913743a35a4e4e38f9dcb04d9b
James <jamespond707@gmail.comm>
2026-04-08 09:09:58 +0700

    feat: implement real-time seat selection view with Supabase integration

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-
+import ToastContainer from '@/components/ui/toast/ToastContainer.vue'
 </script>
 
 <template>
   <router-view />
+  <ToastContainer />
 </template>
 
 <style scoped></style>
